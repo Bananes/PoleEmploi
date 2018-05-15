375b4224fd14a18b5608e818022a74b069547d0a
Spokeek <spokeek@gmail.com>
2018-05-15 10:12:53 +0200

    premiers documents du front portail

---
+++ frontPortail/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.listen(3000)
