0ea0a6a05b491089cf428de839d260f2e7bec3a1
Hugo <hugo.le-dall@etu.univ-nantes.fr>
2018-05-15 10:46:22 +0200

    First commit for back api

---
+++ backData/PoleEmploi/src/main/java/com/poleemploi/back/PoleEmploiApplication.java
@@ -0,0 +1,12 @@
+package com.poleemploi.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoleEmploiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoleEmploiApplication.class, args);
+	}
+}
