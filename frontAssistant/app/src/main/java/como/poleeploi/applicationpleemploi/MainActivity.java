6520b1309648f44ec4776505dfa1df80ff1c4215
khaled44000 <fkiri44@outlook.fr>
2018-05-15 17:03:19 +0200

    ajout de notification

--- frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/MainActivity.java
+++ frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/MainActivity.java
@@ -5,20 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-
+import android.os.Bundle;
+import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import android.widget.Toast;
-
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            mTextMessage.setText("");
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText("Liste des utilisateurs qui ont besoin d'aide :");
+                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -56,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void notification(View view) {
+        addNotification();
+    }
 
-
+    private void addNotification()
+    {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Message non lu")   //this is the title of notification
+                        .setColor(101)
+                        .setContentText("Vous avez un message non lu.");   //this is the message showed in notification
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
 }
---
+++ frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/NotificationBar.java
@@ -0,0 +1,15 @@
+package como.poleeploi.applicationpleemploi;
+import android.os.Bundle;
+import android.app.Activity;
+public class NotificationBar extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_notification_bar);
+
+    }
+
+}
+
