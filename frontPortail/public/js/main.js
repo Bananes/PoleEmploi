c1e19dcafab35d9bd5069d548f2f33857c70f620
Spokeek <spokeek@gmail.com>
2018-05-16 09:36:12 +0200

    clean js

--- frontPortail/public/js/main.js
+++ frontPortail/public/js/main.js
@@ -1,17 +1,17 @@
 const toogleHelper = () => {
-	const div = document.getElementById('helper')
-	const classes = {
-		'large': 'small',
-		'small': 'large'
-	}
-	div.classList.forEach(c => {
-		if(Object.keys(classes).includes(c)){
-			div.classList.replace(c, classes[c])
-		}
-	})
+  const div = document.getElementById('helper')
+  const classes = {
+    'large': 'small',
+    'small': 'large'
+  }
+  div.classList.forEach(c => {
+    if (Object.keys(classes).includes(c)) {
+      div.classList.replace(c, classes[c])
+    }
+  })
 }
 
 const showHelp = () => {
-	const div = document.getElementById('helper-container')
-	div.classList.add('open')
+  const div = document.getElementById('helper-container')
+  div.classList.add('open')
 }
