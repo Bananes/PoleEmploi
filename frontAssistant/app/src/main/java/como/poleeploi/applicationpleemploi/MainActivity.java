822f51a7513562f69ade0c8fb727d0769b2f1b91
khaled44000 <fkiri44@outlook.fr>
2018-05-17 09:35:00 +0200

    GET DATA

--- frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/MainActivity.java
+++ frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/MainActivity.java
@@ -1,46 +1,37 @@
 package como.poleeploi.applicationpleemploi;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.view.View;
+
 import android.widget.TextView;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.view.View;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import android.util.Log;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Button;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
-
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private List lstItems = new LinkedList();
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,28 +39,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // connexion au serveur et recupérer les données
+        runThread();
     }
 
+    private void runThread() {
+       mTextMessage.setText(" \n Vous n'êtes pas connecté aux serveur   \n");
+        new Thread() {
+            public void run() {
+                Socket socket = null;
+                InetAddress group = null;
 
-    public void notification(View view) {
-        addNotification();
+                try {
+
+                      //  mTextMessage.append("  connection au serveur \n");
+
+                        socket = new Socket("192.168.0.5", 8181);
+                      mTextMessage.setText("  Vous êtes connecté \n");
+
+                        BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        while (true) {
+                       //   String dada = "{\"level\":2,\"id\":\"123\",\"type\":\"Time\"}";
+                            String data =    bf.readLine().toString();
+                            // affichege des donnees
+                            dispalyData(data);
+                        //mTextMessage.append(data);
+
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.toString());
+                    mTextMessage.append("  error  " + e.toString());
+                } finally {
+                    if (socket == null) {
+                        //runThread();
+                    }
+                }
+            }
+        }.start();
     }
 
-    private void addNotification()
-    {
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("Message non lu")   //this is the title of notification
-                        .setColor(101)
-                        .setContentText("Vous avez un message non lu.");   //this is the message showed in notification
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
-        // Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
+    private void dispalyData( String data) {
+
+        try {
+            JSONObject jsnobject = new JSONObject(data);
+            String strLveel = jsnobject.getString("level");
+            String strId = jsnobject.getString("id");
+            String strType = jsnobject.getString("type");
+
+            // Test
+            /*
+            mTextMessage.append("  strLveel   " + strLveel + " \n");
+            mTextMessage.append("  strId   " + strId + " \n");
+            mTextMessage.append("  strType   " + strType + " \n");
+            */
+
+            ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.container);
+            ConstraintSet set = new ConstraintSet();
+            set.clone(layout);
+
+            ListView lv = (ListView) findViewById(R.id.list);
+
+            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1, lstItems);
+            lstItems.add("La poste numéro " + strId + " à besoin d'aide ");
+
+            lv.setAdapter(adapter);
+
+        } catch (JSONException jex) {
+            mTextMessage.append(jex.toString());
+            jex.printStackTrace();
+        }
     }
 }
