80358c466067dd606116edfcbb31db20542b4cac
khaled44000 <fkiri44@outlook.fr>
2018-05-17 13:53:43 +0200

    Mise à jour de la récupération des données

--- frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/MainActivity.java
+++ frontAssistant/app/src/main/java/como/poleeploi/applicationpleemploi/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-import android.util.Log;
 
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -22,15 +25,18 @@ import java.io.InputStreamReader;
 
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
-
+import java.io.PrintWriter;
 import org.json.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.net.*;
+import java.io.*;
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
-    private List lstItems = new LinkedList();
+    private List<String> lstItems = new LinkedList<String>();
 
 
     @Override
@@ -38,47 +44,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+       mTextMessage = (TextView) findViewById(R.id.message);
         // connexion au serveur et recupérer les données
-        runThread();
+      runThread();
+        //String data = "{\"level\":2,\"id\":\"123\",\"type\":\"Time\"}\n";
+       //dispalyData(data);
     }
 
     private void runThread() {
-       mTextMessage.setText(" \n Vous n'êtes pas connecté aux serveur   \n");
+      // mTextMessage.setText(" \n Vous n'êtes pas connecté aux serveur   \n");
         new Thread() {
             public void run() {
+                String hostname = "192.168.43.230";
+                int intPort = 8181;
                 Socket socket = null;
-                InetAddress group = null;
-
                 try {
-
                       //  mTextMessage.append("  connection au serveur \n");
+                    socket = new Socket(hostname, intPort);
+                } catch (UnknownHostException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                try{
 
-                        socket = new Socket("192.168.0.5", 8181);
-                      mTextMessage.setText("  Vous êtes connecté \n");
-
-                        BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        while (true) {
+                    PrintWriter  out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                    BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    //mTextMessage.setText("  Vous êtes connecté \n");
+                    String data;
+                        while (!bf.ready()  ) {
                        //   String dada = "{\"level\":2,\"id\":\"123\",\"type\":\"Time\"}";
-                            String data =    bf.readLine().toString();
+
+                            data =  bf.readLine();
                             // affichege des donnees
                             dispalyData(data);
                         //mTextMessage.append(data);
-
                     }
                 } catch (IOException e) {
-                    System.out.println(e.toString());
-                    mTextMessage.append("  error  " + e.toString());
-                } finally {
-                    if (socket == null) {
-                        //runThread();
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    //Closing the socket
+                    try
+                    {
+                        socket.close();
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
                     }
                 }
+
             }
         }.start();
     }
 
-    private void dispalyData( String data) {
+    private void dispalyData ( String data) {
 
         try {
             JSONObject jsnobject = new JSONObject(data);
@@ -97,16 +119,29 @@ public class MainActivity extends AppCompatActivity {
             ConstraintSet set = new ConstraintSet();
             set.clone(layout);
 
-            ListView lv = (ListView) findViewById(R.id.list);
+            //ListView lv = (ListView) findViewById(R.id.list);
+            //lstItems.add("La poste numéro " + strId + " à besoin d'aide ");
+            //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+            //        android.R.layout.simple_list_item_1, lstItems);
 
-            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                    android.R.layout.simple_list_item_1, lstItems);
-            lstItems.add("La poste numéro " + strId + " à besoin d'aide ");
 
-            lv.setAdapter(adapter);
+            final String text = "La poste numéro " + strId + " à besoin d'aide ";
+
+            layout.post(new Runnable() {
+                @Override
+                public void run() {
+                    ListView DynamicListView = new ListView(getApplicationContext());
+                    lstItems.add(text);
+                    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.listTemp);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                            (MainActivity.this, android.R.layout.simple_list_item_1, lstItems);
+
+                    DynamicListView.setAdapter(adapter);
+                    linearLayout.addView(DynamicListView);
+                }
+            });
 
         } catch (JSONException jex) {
-            mTextMessage.append(jex.toString());
             jex.printStackTrace();
         }
     }
